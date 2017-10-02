0d4fae0a2175cd4ef1826b5e502ea83bd0d15c54
AlanMo <Alan.Mo@seed.space>
2017-10-01 20:17:16 -0700

    redis setup

--- modules/config.js
+++ modules/config.js
@@ -6,33 +6,6 @@ var _ = require('../libs/underscore-min.js');
 
 var app_config = {};
 
-app_config.mongoDBOptions = function(env){
-    var host = '';
-    var port = 27017;
-    var database = '';
-    var user = '';
-    var password = '';
-
-    switch(env) {
-        case 'development':
-            host = 's1.thebusy.net';
-            database = 'group_orders';
-            user = local.mongodb.user;
-            password = local.mongodb.password;
-            url = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + database;
-            break;
-        case 'production':
-            host = '172.17.0.1';
-            database = 'group_orders';
-            user = local.mongodb.user;
-            password = local.mongodb.password;
-            url = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + database;
-            break;
-        default:
-    }
-    return {database: database, host: host, port: port, user: user, password: password, url: url};
-};
-
 app_config.initDB = function(env){
     var host = '';
     var database = '';
@@ -43,15 +16,15 @@ app_config.initDB = function(env){
 
     switch(env) {
         case 'development':
-            host = 'xpzdb1.cno0bmfepeqp.us-east-1.rds.amazonaws.com';
-            database = 'tuangou';
+            host = 'menucraftdb.cijjg23l7jnd.us-west-1.rds.amazonaws.com';
+            database = 'menucraft_db';
             user = local.mysql.user;
             password = local.mysql.password;
             charset = 'utf8mb4';
             break;
         case 'production':
-            host = 'xpzdb1.cno0bmfepeqp.us-east-1.rds.amazonaws.com';
-            database = 'tuangou';
+            host = 'menucraftdb.cijjg23l7jnd.us-west-1.rds.amazonaws.com';
+            database = 'menucraft_db';
             user = local.mysql.user;
             password = local.mysql.password;
             charset = 'utf8mb4';
@@ -66,7 +39,7 @@ app_config.initRedis = function (env) {
     var redisObj = {};
     switch(env) {
         case 'development':
-            redisObj.host = 's1.thebusy.net';
+            redisObj.host = '127.0.0.1';
             redisObj.port = '6379';
             redisObj.password = local.redis.password;
             break;
